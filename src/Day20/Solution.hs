83d107202c7d4ce551d2a14178bfe3c761b30d93
Jiri Lojda <lojdaj@seznam.cz>
2023-04-16 18:30:00 +0200

    Solve day20 task2

--- src/Day20/Solution.hs
+++ src/Day20/Solution.hs
@@ -9,39 +9,67 @@ import Text.Megaparsec.Char qualified as MP
 import Text.Megaparsec.Char.Lexer qualified as MP.L
 
 import Control.Monad qualified as Monad
-import Data.Function ((&))
+import Data.List qualified as List
 import Data.Sequence qualified as Seq
 import Utils (Parser, loadAndParseFile)
 
-task2 :: IO String
-task2 = pure "Not Implemented"
+decryptionKey :: Int
+decryptionKey = 811589153
+
+task2 :: IO (Maybe Int)
+task2 = do
+  fileNums <- Seq.fromList <$> Utils.loadAndParseFile "src/Day20/data.txt" fileParser
+  let inputWithDecryptionKey = fmap (* decryptionKey) fileNums
+  let mixed = List.foldl' (\currentSeq _ -> performMixing getNextIndexForMultiPass currentSeq) (convertToFileEntries inputWithDecryptionKey) [1 .. 10 :: Int]
+  pure (calculateResult $ convertFromFileEntries mixed)
 
 task1 :: IO (Maybe Int)
 task1 = do
   fileNums <- Seq.fromList <$> Utils.loadAndParseFile "src/Day20/data.txt" fileParser
-  let mixed = performMixing fileNums
-  pure $ do
-    indexOf0 <- Seq.findIndexL (== 0) mixed
-    coord1 <- Seq.lookup (wrapIndex mixed (indexOf0 + 1000)) mixed
-    coord2 <- Seq.lookup (wrapIndex mixed (indexOf0 + 2000)) mixed
-    coord3 <- Seq.lookup (wrapIndex mixed (indexOf0 + 3000)) mixed
-    pure (coord1 + coord2 + coord3)
+  let mixed = performMixing getNextIndexForSinglePass $ convertToFileEntries fileNums
+  pure (calculateResult $ convertFromFileEntries mixed)
+
+calculateResult :: Seq Int -> Maybe Int
+calculateResult items = do
+  indexOf0 <- Seq.findIndexL (== 0) items
+  coord1 <- Seq.lookup (wrapIndex items (indexOf0 + 1000)) items
+  coord2 <- Seq.lookup (wrapIndex items (indexOf0 + 2000)) items
+  coord3 <- Seq.lookup (wrapIndex items (indexOf0 + 3000)) items
+  pure (coord1 + coord2 + coord3)
 
 data FileEntry = MkFileEntry {index :: Int, value :: Int} deriving (Show)
+
+convertToFileEntries :: Seq Int -> Seq FileEntry
+convertToFileEntries input = Seq.zipWith (\index value -> MkFileEntry{index, value}) (Seq.fromList $ take (Seq.length input) [0 ..]) input
+
+convertFromFileEntries :: Seq FileEntry -> Seq Int
+convertFromFileEntries = fmap (.value)
+
 data ProcessState = MkProcessState {sequencePos :: Int, originalSequencePos :: Int} deriving (Show)
 
-performMixing :: Seq Int -> Seq Int
-performMixing input =
-  input
-    & Seq.zipWith (\index value -> MkFileEntry{index, value}) (Seq.fromList $ take (Seq.length input) [0 ..])
-    & process (MkProcessState{sequencePos = 0, originalSequencePos = 0})
-    & fmap (.value)
+data NextIndexContext = MkNextIndexContext {currentIndex :: Int, nextIndexInOriginalSeq :: Int, newEntryIndex :: Int, newSeq :: Seq FileEntry}
+
+-- for task one leading to O(n * log n) complexity (can only handle 1 mixing pass)
+getNextIndexForSinglePass :: NextIndexContext -> Maybe Int
+getNextIndexForSinglePass ctx
+  | ctx.newEntryIndex <= ctx.currentIndex && ctx.currentIndex >= Seq.length ctx.newSeq - 1 = Nothing
+  | ctx.newEntryIndex <= ctx.currentIndex = Just (ctx.currentIndex + 1)
+  | otherwise = Just ctx.currentIndex
+
+-- for task two leasing to O(n * n * log n) complexity (handles multiple mixing passes)
+getNextIndexForMultiPass :: NextIndexContext -> Maybe Int
+getNextIndexForMultiPass ctx = Seq.findIndexL ((== ctx.nextIndexInOriginalSeq) . (.index)) ctx.newSeq
+
+performMixing :: (NextIndexContext -> Maybe Int) -> Seq FileEntry -> Seq FileEntry
+performMixing getNextIndex = process (MkProcessState{sequencePos = 0, originalSequencePos = 0})
  where
   process :: ProcessState -> Seq FileEntry -> Seq FileEntry
   process _ Seq.Empty = Seq.Empty
   process state currentSeq
     | state.sequencePos >= Seq.length currentSeq = currentSeq
-    | Just entry <- getExpectedEntry state currentSeq = uncurry process (moveEntry entry state currentSeq)
+    | Just entry <- getExpectedEntry state currentSeq = case moveEntry entry state currentSeq of
+        (Nothing, finalSeq) -> finalSeq
+        (Just nextState, nextSeq) -> process nextState nextSeq
     | otherwise = process (state{sequencePos = state.sequencePos + 1}) currentSeq
 
   getExpectedEntry :: ProcessState -> Seq FileEntry -> Maybe FileEntry
@@ -50,12 +78,16 @@ performMixing input =
     Monad.guard (entry.index == state.originalSequencePos)
     pure entry
 
-  moveEntry :: FileEntry -> ProcessState -> Seq FileEntry -> (ProcessState, Seq FileEntry)
-  moveEntry entry state currentSeq = (MkProcessState{sequencePos = newSeqPos, originalSequencePos = state.originalSequencePos + 1}, Seq.insertAt newIndex entry seqWithoutEntry)
+  moveEntry :: FileEntry -> ProcessState -> Seq FileEntry -> (Maybe ProcessState, Seq FileEntry)
+  moveEntry entry state currentSeq = (newState, finalSeq)
    where
     seqWithoutEntry = Seq.deleteAt state.sequencePos currentSeq
+    finalSeq = Seq.insertAt newIndex entry seqWithoutEntry
     newIndex = calculateNewIndex entry state seqWithoutEntry
-    newSeqPos = if newIndex <= state.sequencePos then state.sequencePos + 1 else state.sequencePos
+    newSeqPos = getNextIndex MkNextIndexContext{currentIndex = state.sequencePos, nextIndexInOriginalSeq = state.originalSequencePos + 1, newEntryIndex = newIndex, newSeq = finalSeq}
+    newState = case newSeqPos of
+      Nothing -> Nothing
+      Just sequencePos -> Just MkProcessState{sequencePos, originalSequencePos = state.originalSequencePos + 1}
 
   calculateNewIndex :: FileEntry -> ProcessState -> Seq FileEntry -> Int
   calculateNewIndex entry state currentSeq = wrapIndex currentSeq (state.sequencePos + entry.value)
